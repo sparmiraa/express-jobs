e6f0c040ca61c6617b11aceeba6e66eff8d62e21
sparmira <aliceops09@gmail.com>
2026-02-24 14:43:10 +0600

    [tsk-#SPAR-5] Реализовать минимальный функционал пользователей (правка кое какая)

--- src/routes/employerRoutes.js
+++ src/routes/employerRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import AuthMiddleware from "../middlewares/authMiddleware.js";
-import validateMiddleware from "../middlewares/validateMiddleware.js";
+import {validateMiddleware} from "../middlewares/validateMiddleware.js";
 import employerController from "../controllers/employerController.js";
 
 import { updateEmployerBioSchema } from "../schemas/employerSchemas/updateBioSchema.js";
--- src/schemas/employerSchemas/updateBioSchema.js
+++ src/schemas/employerSchemas/updateBioSchema.js
@@ -1,10 +1,7 @@
 import * as yup from "yup";
 
-export const updateEmployerBioSchema = yup
-  .object({
-    bio: yup.string().max(2000, "Максимум 2000 символов").required(),
+export const updateEmployerBioSchema = yup.object({
+  bio: yup.string().max(2000, "Максимум 2000 символов").required(),
 
-    employeesCount: yup.number().integer().min(0).required(),
-  })
-  .noUnknown();
-s;
+  employeesCount: yup.number().integer().min(0).required(),
+});
